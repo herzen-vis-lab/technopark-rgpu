827917f5509ef94442c4238b12a71d6f2e18f5fc
Kostin Pavel <50001467+KostinP@users.noreply.github.com>
2026-06-24 18:21:27 +0300

    Change FAQ title span class to empty string

--- template-parts/faq.php
+++ template-parts/faq.php
@@ -20,7 +20,7 @@ if ($query->have_posts()):
                     data-accordion-target="#accordion-card-body-<?php echo $counter; ?>" 
                     aria-expanded="false" 
                     aria-controls="accordion-card-body-<?php echo $counter; ?>">
-                    <span class="text-h3"><?php the_title(); ?></span>
+                    <span class=""><?php the_title(); ?></span>
                     <svg data-accordion-icon class="tp-faq-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m5 15 7-7 7 7"/>
                     </svg>
